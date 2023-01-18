5a74f3dde852f1b7659a29aa4fa61da10836cf01
Hamza Khan <Hamza.khan@santex.com.pk>
2023-01-18 13:37:23 +0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace iSOL_Enterprise.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
