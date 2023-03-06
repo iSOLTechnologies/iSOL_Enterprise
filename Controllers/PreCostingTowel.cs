10852b1e41ee5ec0eaadab548f84a11e5b1b3a75
Gitreef Akram <gitreefa12@gmail.com>
2023-03-06 19:07:38 +0500

    Pre costing towel form added not completed
    Pre costing towel form added not completed

---
+++ Controllers/PreCostingTowel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace iSOL_Enterprise.Controllers
+{
+    public class PreCostingTowel : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
