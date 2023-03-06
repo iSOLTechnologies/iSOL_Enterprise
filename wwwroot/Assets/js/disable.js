8f6985e0134ea1d40da6e8a17e7199e22e58dd9b
Gitreef Akram <gitreefa12@gmail.com>
2023-03-06 11:42:53 +0500

    on enter key press trigger login done

--- wwwroot/Assets/js/disable.js
+++ wwwroot/Assets/js/disable.js
@@ -92,7 +92,7 @@ setTimeout(myfun, 5000);
 
 	// Disable all inspect buttons
 	document.onkeydown = function(e) {
- if(event.keyCode == 123) {
+		if (e.keyCode == 123) {
     return false;
  }
  if(e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
