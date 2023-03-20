318f49526c28d29a3e5b08ba106160ebdbae642b
Muhammad Annas Raza <annasraza17@gmail.com>
2023-03-20 14:10:51 +0500

    Done Invenentory Add

---
+++ Controllers/Inventory/ItemMasterDataController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace iSOL_Enterprise.Controllers.Inventory
+{
+    public class ItemMasterDataController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ItemMasterDataMaster()
+        {
+            return View();
+        }
+    }
+}
