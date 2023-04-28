d82976594b9daf1fd1702ed504203c814145e2fe
Gitreef Akram <gitreefa12@gmail.com>
2023-04-28 13:02:30 +0500

    missing file added

---
+++ isol_Enterprise/Models/Logs/OITL.cs
@@ -0,0 +1,18 @@
+﻿namespace iSOL_Enterprise.Models.Logs
+{
+    public class OITL
+    {
+
+        public int LogEntry { get; set; }
+        public int ID { get; set; }
+        public int DocLine { get; set; }
+        public int DocType { get; set; }
+        public dynamic BaseType { get; set; }
+        public decimal Quantity { get; set; }
+        public string CardCode { get; set; }
+        public string CardName { get; set; }
+        public string ItemCode { get; set; }
+        public string ItemName { get; set; }
+        public DateTime DocDate { get; set; }
+    }
+}
