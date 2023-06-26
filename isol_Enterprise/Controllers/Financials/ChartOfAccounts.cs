87a2d91ea7c70446986587a4c15d1ca3bd375e17
Gitreef akram <gitreefa12@gmail.com>
2023-06-26 19:07:26 +0500

    COA added and tree working done wrt drawers

---
+++ isol_Enterprise/Controllers/Financials/ChartOfAccounts.cs
@@ -0,0 +1,67 @@
+﻿using iSOL_Enterprise.Dal.Financials;
+using iSOL_Enterprise.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace iSOL_Enterprise.Controllers.Financials
+{
+    public class ChartOfAccounts : Controller
+    {
+        public IActionResult Index()
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(HttpContext.Session.GetInt32("UserId").ToString()) || string.IsNullOrEmpty(HttpContext.Request.Query["Source"]))
+                {
+                    return RedirectToAction("index", "Home");
+                }
+
+
+                return View();
+
+
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("index", "Home");
+
+            }
+        }
+        public IActionResult ChartOfAccountsMaster()
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(HttpContext.Session.GetInt32("UserId").ToString()) || string.IsNullOrEmpty(HttpContext.Request.Query["Source"]))
+                {
+                    return RedirectToAction("index", "Home");
+                }
+
+                ChartOfAccountDal dal = new ();
+                ViewBag.Drawers = dal.GetDrawers();
+                return View();
+
+
+            }
+            catch (Exception ex)
+            {
+                return RedirectToAction("index", "Home");
+
+            }
+        }
+        public IActionResult Getlevels(string drawer)
+        {
+            ResponseModels models = new ResponseModels();
+            ChartOfAccountDal dal = new ();
+            try
+            {
+                models.Data = dal.GetLevels(drawer);
+                return Json(JsonConvert.SerializeObject(models.Data));
+            }
+            catch (Exception ex)
+            {
+
+                return Json(models);
+            }
+        }
+    }
+}
---
+++ isol_Enterprise/Dal/Financials/ChartOfAccountDal.cs
@@ -0,0 +1,89 @@
+﻿using iSOL_Enterprise.Models;
+using SqlHelperExtensions;
+using System.Data;
+using System.Data.SqlClient;
+namespace iSOL_Enterprise.Dal.Financials
+{
+    public class ChartOfAccountDal
+    {
+
+        public List<ListModel> GetDrawers()
+        {
+
+
+            string GetQuery = "select AcctCode,AcctName  from OACT where FatherNum is null";
+
+            List<ListModel> list = new List<ListModel>();
+
+            using (var rdr = SqlHelper.ExecuteReader(SqlHelper.defaultSapDB, CommandType.Text, GetQuery))
+            {
+                while (rdr.Read())
+                {
+                    list.Add(new ListModel()
+                    {
+                        ValueString = rdr["AcctCode"].ToString(),
+                        Text = rdr["AcctName"].ToString()
+
+                    });
+                }
+            }
+            return list;
+        }
+
+        public List<TreeModel> GetLevels(string drawer = null)
+        {
+
+            CommonDal cdal = new();
+
+            List<TreeModel> Pages = new List<TreeModel>();
+            string query = @"select AcctCode,AcctName from OACT where FatherNum =" + drawer;
+
+            using (var rdr = SqlHelper.ExecuteReader(SqlHelper.defaultSapDB, CommandType.Text,query))
+            {
+                while (rdr.Read())
+                {
+                    TreeModel model = new TreeModel();
+                    string AcctCode = rdr["AcctCode"].ToString();
+
+                    model.id = AcctCode;
+                    model.text = "<span class='text-success'>" + AcctCode + " - " + rdr["AcctName"].ToString() + "</span>";
+                    model.@checked = false;
+                    model.population -= null;
+                    model.flagUrl = null;                    
+                    model.children = GetLevelChilds(AcctCode);
+                    Pages.Add(model);
+                }
+            }
+            return Pages;
+        }
+
+        private List<TreeModel> GetLevelChilds(string AcctCode)
+        {
+            CommonDal cdal = new();
+
+            
+            List<TreeModel> Pages = new List<TreeModel>();
+            string query = @"select AcctCode,AcctName from OACT where FatherNum ='" + AcctCode + "'";
+            using (var rdr = SqlHelper.ExecuteReader(SqlHelper.defaultSapDB, CommandType.Text, query))
+            {
+                while (rdr.Read())
+                {
+                    TreeModel model = new TreeModel();
+                    string AcctCode1 = rdr["AcctCode"].ToString();
+
+                    model.id = AcctCode1;
+                    model.text =  AcctCode1 + " - " + rdr["AcctName"].ToString();
+                    model.@checked = false;
+                    model.population -= null;
+                    model.flagUrl = null;
+                    model.children = GetLevelChilds(AcctCode1);
+                    if (model.children.Count > 0)
+                        model.text = "<span class='text-success'>" + AcctCode1 + " - " + rdr["AcctName"].ToString() + "</span>";
+                    
+                    Pages.Add(model);
+                }
+            }
+            return Pages;
+        }
+    }
+}
