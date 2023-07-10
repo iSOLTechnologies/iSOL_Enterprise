4089a7c3ad462d8976c47ce0b16638564b8b1ab7
Gitreef akram <gitreefa12@gmail.com>
2023-07-10 14:47:57 +0500

    approvals bug fixed

--- isol_Enterprise/wwwroot/js/CommonIndex.js
+++ isol_Enterprise/wwwroot/js/CommonIndex.js
@@ -1,7 +1,8 @@
 ﻿
+var UserPageActivityData = "";
 String.prototype.GetUserPageActivity = function () {
     // Custom method logic
-    
+
     return $.ajax({
         url: 'Common/GetUserPageActivity',
         type: 'GET',
@@ -10,42 +11,39 @@ String.prototype.GetUserPageActivity = function () {
         data: { Guid: this },
         success: function (result) {
             console.log(result);
-            
+
         },
         error: function (jqXhr, textStatus, errorMessage) {
             console.log(errorMessage);
         }
     }).done(function (result) {
-        
+
         UpdatePageActivities(result.data);
+        UserPageActivityData = result.data;
     });
 };
-function UpdatePageActivities(data)
-{
+function UpdatePageActivities(data) {
     for (var i = 0; i < data.length; i++) {
 
-        
-        if (data[i].roleActivityTypeCode == 'A' && !data[i].isActive)
-        {
+
+        if (data[i].roleActivityTypeCode == 'A' && !data[i].isActive) {
             $("#btnAdd").addClass("d-none");
         }
 
-        if (data[i].roleActivityTypeCode == 'E' && !data[i].isActive)
-        {
-            
+        if (data[i].roleActivityTypeCode == 'E' && !data[i].isActive) {
             $(".btnEdit").addClass("d-none");
         }
-        if (data[i].roleActivityTypeCode == 'D' && !data[i].isActive)
-        {
+        if (data[i].roleActivityTypeCode == 'D' && !data[i].isActive) {
             $(".btnDelete").addClass("d-none");
         }
-        if (data[i].roleActivityTypeCode == 'V' && !data[i].isActive)
-        {
+        if (data[i].roleActivityTypeCode == 'V' && !data[i].isActive) {
             $(".btnView").addClass("d-none");
         }
-        
+
     }
 }
-
-
+$(document).on('click', '.paginate_button.page-item', function () {
+    console.log('click');
+    UpdatePageActivities(UserPageActivityData);
+});
 
